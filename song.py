0bd773be46754b7792b619ed6f7a5f7890a54368
kuna <kgdgwn98@gmail.com>
2015-03-24 23:46:55 +0900

    added imgtl api, capture api, static file, stabilize, but scarce image quality...

--- song.py
+++ song.py
@@ -3,31 +3,128 @@
 import urllib
 import json
 
-def getSPinfo(user, lv):
-	url = ('http://json.iidx.me/%s/sp/level/%d/' % (user, lv))
+def getiidxinfo(user, type, lv):
+	url = ('http://json.iidx.me/%s/%s/level/%d/' % (user, type, lv))
 	res = urllib.urlopen(url)
 	data = json.loads(res.read())
 	return data
 
 # dict: category -> songs[] (song)
 def getCSVdata(csvpath):
-	f = open(csvfile, 'rb')
+	def processCode(code):
+		# H/A/N confine command, ALL command
+		attr = {}
+		if (code == "ALL"):
+			attr = {'option': 'ALL'}
+		elif (not code[-1:].isdigit()):
+			attr = {'id': code[:-1], 'option':code[-1:]}
+		else:
+			attr = {'id': code}
+		return attr
+
+	f = open(csvpath, 'rb')
 	lines = f.readlines()
-	f.close()
+	f.close() 
 	csvdata = []
 	for l in lines:
+		l = l.decode('utf8')
+		l = l.replace('\n', '').replace('\r', '')
 		arr = l.split('\t')
-		songs = map(lambda x: {'id': x}, arr[1:])
-		csvdata.append({'category': arr[0], 'songs': arr[1:]})
+		arr = filter(None, arr)	# remove empty array
+		songs = map(processCode, arr[1:])
+		csvdata.append({'category': arr[0], 'songs': songs})
 	return csvdata
 
 # dict: category -> songs[] (title, code, clear, ex, ...)
 def processCSV(musicdata, csvdata):
-	for category in csvdata:
-		for song in category.songs:
-			record = musicdata.get('id', song.id)
-			if (record):
-				category.songs.get('id', song.id) = record
-	return csvdata
+	def getdict(val):
+		for music in musicdata:
+			if (music['data']['id'] == val['id']):
+				if (('option' in val and val['option'] == music['data']['diff']) or \
+					'option' not in val):
+					musicdata.remove(music);
+					return music
+		return None		
 
-#print getSPinfo('kuna', 12)
+	# preprocess musicdata
+	for music in musicdata:
+		# ex: dpa -> A
+		music['data']['diff'] = music['data']['diff'][-1:].upper()
+		# change PLAY
+		clear = int(music['clear'])
+		if (clear == 0):
+			music['clearstring'] = u'NO_PLAY'
+		elif (clear == 1):
+			music['clearstring'] = u'FAILED'
+		elif (clear == 2):
+			music['clearstring'] = u'ASSIST_CLEAR'
+		elif (clear == 3):
+			music['clearstring'] = u'EASY_CLEAR'
+		elif (clear == 4):
+			music['clearstring'] = u'CLEAR'
+		elif (clear == 5):
+			music['clearstring'] = u'HARD_CLEAR'
+		elif (clear == 6):
+			music['clearstring'] = u'EX-HARD_CLEAR'
+		elif (clear == 7):
+			music['clearstring'] = u'FULL_COMBO'
+
+		# make rate
+		if (music['score'] == None):
+			music['rate'] = 0
+		else:
+			music['rate'] = music['score'] / float(music['data']['notes']) / 2 * 100
+		
+		# make rank
+		if (music['rate'] >= 8.0/9*100):
+			music['rank'] = u"AAA"
+		elif (music['rate'] >= 7.0/9*100):
+			music['rank'] = u"AA"
+		elif (music['rate'] >= 6.0/9*100):
+			music['rank'] = u"A"
+		elif (music['rate'] >= 5.0/9*100):
+			music['rank'] = u"B"
+		elif (music['rate'] >= 4.0/9*100):
+			music['rank'] = u"C"
+		elif (music['rate'] >= 3.0/9*100):
+			music['rank'] = u"D"
+		elif (music['rate'] >= 2.0/9*100):
+			music['rank'] = u"E"
+		else:
+			music['rank'] = u"F"
+
+	# process r
+	# this contains dictonaries, {category, songs=[]}
+	r = []
+	for category in csvdata:
+		catearray = {}
+		catearray['category'] = category['category']
+		catearray['songs'] = []
+		# if songid is ALL, then add all left songs.
+		for song in category['songs']:
+			if ('option' in song and song['option'] == 'ALL'):
+				catearray['songs'] = musicdata
+			else:
+				record = getdict(song)
+				if (record):
+					# write difficulty if same song exists
+					# ex: gigadelic [A]
+					if ('option' in song):
+						record['data']['title'] += " [" + record['data']['diff'] + "]"
+					# add current song in current category
+ 					catearray['songs'].append(record)
+				else:
+					# this is for debugging.
+					# if this message shows, then it's time to update data(txt) file.
+					print "unknown song: %s" % (song['id'])
+
+		# check out category lamp (clear)
+		catearray['categoryclear'] = 7
+		catearray['categoryclearstring'] = u'FULL_COMBO'
+		for song in catearray['songs']:
+			if (song['clear'] < catearray['categoryclear']):
+				catearray['categoryclear'] = song['clear']
+				catearray['categoryclearstring'] = song['clearstring']
+
+		r.append(catearray)
+	return r
--- web.py
+++ web.py
@@ -1,7 +1,56 @@
+#-*- coding: utf-8 -*-
 # start web server
 from flask import Flask, render_template
 import song, view
 import env
+
+from datetime import timedelta
+from flask import make_response, request, current_app
+from functools import update_wrapper
+
+import base64
+
+def crossdomain(origin=None, methods=None, headers=None,
+                max_age=21600, attach_to_all=True,
+                automatic_options=True):
+    if methods is not None:
+        methods = ', '.join(sorted(x.upper() for x in methods))
+    if headers is not None and not isinstance(headers, basestring):
+        headers = ', '.join(x.upper() for x in headers)
+    if not isinstance(origin, basestring):
+        origin = ', '.join(origin)
+    if isinstance(max_age, timedelta):
+        max_age = max_age.total_seconds()
+
+    def get_methods():
+        if methods is not None:
+            return methods
+
+        options_resp = current_app.make_default_options_response()
+        return options_resp.headers['allow']
+
+    def decorator(f):
+        def wrapped_function(*args, **kwargs):
+            if automatic_options and request.method == 'OPTIONS':
+                resp = current_app.make_default_options_response()
+            else:
+                resp = make_response(f(*args, **kwargs))
+            if not attach_to_all and request.method != 'OPTIONS':
+                return resp
+
+            h = resp.headers
+
+            h['Access-Control-Allow-Origin'] = origin
+            h['Access-Control-Allow-Methods'] = get_methods()
+            h['Access-Control-Max-Age'] = str(max_age)
+            if headers is not None:
+                h['Access-Control-Allow-Headers'] = headers
+            return resp
+
+        f.provide_automatic_options = False
+        return update_wrapper(wrapped_function, f)
+    return decorator
+
 
 app = Flask(__name__)
 
@@ -9,10 +58,162 @@ app = Flask(__name__)
 def main():
 	return render_template('main.html')
 
-@app.route('/iidx/sp/<user>/<int:level>')
-def iidxsp(user, level):
-	player = song.getSPinfo(user, level)
-	return render_template('common.html', datas=player.musicdata)
+def render_score(player, score, mode_, title_):
+	def getdanstring(dan):
+		if (dan == 1):
+			return u"-"
+		elif (dan == 2):
+			return u"七級"
+		elif (dan == 3):
+			return u"六級"
+		elif (dan == 4):
+			return u"五級"
+		elif (dan == 5):
+			return u"四級"
+		elif (dan == 6):
+			return u"三級"
+		elif (dan == 7):
+			return u"二級"
+		elif (dan == 8):
+			return u"一級"
+		elif (dan == 9):
+			return u"初段"
+		elif (dan == 10):
+			return u"二段"
+		elif (dan == 11):
+			return u"三段"
+		elif (dan == 12):
+			return u"四段"
+		elif (dan == 13):
+			return u"五段"
+		elif (dan == 14):
+			return u"六段"
+		elif (dan == 15):
+			return u"七段"
+		elif (dan == 16):
+			return u"八段"
+		elif (dan == 17):
+			return u"九段"
+		elif (dan == 18):
+			return u"十段"
+		elif (dan == 19):
+			return u"皆伝"
+
+	name = player['userdata']['djname']
+	spdan = player['userdata']['spclass']
+	dpdan = player['userdata']['dpclass']
+
+	return render_template('common.html', mode=mode_, title=title_, \
+		user={'name': name, 'spdan': getdanstring(spdan), 'spdannum':spdan, 'dpdan': getdanstring(dpdan), 'dpdannum': dpdan},\
+		datas=score)
+
+@app.route('/iidx/sp/<user>/12.7')
+def iidxsp127(user):
+	# clddal.kr
+	# parse web
+	player = song.getiidxinfo(user, 'sp', 12)
+	if ('userdata' not in player or player['status'] != 'success'):
+		abort(404)
+
+	# load data file
+	data = song.getCSVdata("./data/sp.12.7.txt")
+
+	# create score data
+	score = song.processCSV(player['musicdata'], data)
+
+	return render_score(player, score, "sp", "Beatmania IIDX SP lv.12 Rank")
+
+@app.route('/iidx/sp/<user>/12')
+def iidxsp12(user):
+	# http://nozomi.2ch.sc/test/read.cgi/otoge/1405129623/
+	# parse web
+	player = song.getiidxinfo(user, 'sp', 12)
+	if ('userdata' not in player or player['status'] != 'success'):
+		abort(404)
+
+	# load data file
+	data = song.getCSVdata("./data/sp.12.txt")
+
+	# create score data
+	score = song.processCSV(player['musicdata'], data)
+
+	return render_score(player, score, "sp", "Beatmania IIDX SP lv.12 <span style='color:red;'>Hard Guage</span> Rank")
+
+@app.route('/iidx/dp/<user>/12')
+def iidxdp12(user):
+	# parse web
+	player = song.getiidxinfo(user, 'dp', 12)
+	if ('userdata' not in player or player['status'] != 'success'):
+		abort(404)
+
+	# load data file
+	data = song.getCSVdata("./data/dp.12.txt")
+
+	# create score data
+	score = song.processCSV(player['musicdata'], data)
+
+	return render_score(player, score, "dp", "Beatmania IIDX DP lv.12 Rank")
+
+@app.route('/iidx/dp/<user>/11')
+def iidxdp11(user):
+	# parse web
+	player = song.getiidxinfo(user, 'dp', 11)
+	if ('userdata' not in player or player['status'] != 'success'):
+		abort(404)
+
+	# load data file
+	data = song.getCSVdata("./data/dp.11.txt")
+
+	# create score data
+	score = song.processCSV(player['musicdata'], data)
+
+	return render_score(player, score, "dp", "Beatmania IIDX DP lv.11 Rank")
+
+@app.route('/iidx/dp/<user>/10')
+def iidxdp10(user):
+	# parse web
+	player = song.getiidxinfo(user, 'dp', 10)
+	if ('userdata' not in player or player['status'] != 'success'):
+		abort(404)
+
+	# load data file
+	data = song.getCSVdata("./data/dp.10.txt")
+
+	# create score data
+	score = song.processCSV(player['musicdata'], data)
+
+	return render_score(player, score, "dp", "Beatmania IIDX DP lv.10 Rank")
+
+@app.route('/test')
+@crossdomain(origin='https://api.img.tl', headers='Content-Type')
+def test():
+	import json
+	f = open('./data/test', 'rb')
+	player = json.loads(f.read())
+	f.close()
+
+	data = song.getCSVdata("./data/dp.10.txt")
+	score = song.processCSV(player['musicdata'], data)
+
+	return render_score(player, score, "dp", "Beatmania IIDX SP lv.12 Hard Guage Rank")
+
+@app.route('/imgtl', methods=['POST'])
+def imgtl():
+	print "got imgtl request: %s" % request.form['name']
+	filename = request.form['name']
+	pngdata = base64.decodestring(request.form['base64'])
+
+	import requests
+	import urllib2
+	header = {'X-IMGTL-TOKEN': '87b8e0cac380e5cfc190101ff70ab6a1'}
+	r = requests.post('https://api.img.tl/upload', data={'desc': '', 'filename': filename}, \
+		files={'file': pngdata}, headers=header)
+	"""print r.status_code
+	print r.text
+	f = open('test.png', 'wb')
+	f.write(pngdata)
+	f.close()"""
+	return r.text
 
 def init():
 	if (env.TESTING):
@@ -20,4 +221,4 @@ def init():
 
 if __name__ == '__main__':
 	init()
-	app.run(host='127.0.0.1', port=80)
+	app.run(host='0.0.0.0', port=80)
