4a9f73f85c0340d1e4c4ae48cbc50e63d610e90b
kuna <kgdgwn98@gmail.com>
2015-03-25 00:00:30 +0900

    removed crossdomain code & useless file

--- web.py
+++ web.py
@@ -4,53 +4,9 @@ from flask import Flask, render_template
 import song, view
 import env
 
-from datetime import timedelta
 from flask import make_response, request, current_app
-from functools import update_wrapper
 
 import base64
-
-def crossdomain(origin=None, methods=None, headers=None,
-                max_age=21600, attach_to_all=True,
-                automatic_options=True):
-    if methods is not None:
-        methods = ', '.join(sorted(x.upper() for x in methods))
-    if headers is not None and not isinstance(headers, basestring):
-        headers = ', '.join(x.upper() for x in headers)
-    if not isinstance(origin, basestring):
-        origin = ', '.join(origin)
-    if isinstance(max_age, timedelta):
-        max_age = max_age.total_seconds()
-
-    def get_methods():
-        if methods is not None:
-            return methods
-
-        options_resp = current_app.make_default_options_response()
-        return options_resp.headers['allow']
-
-    def decorator(f):
-        def wrapped_function(*args, **kwargs):
-            if automatic_options and request.method == 'OPTIONS':
-                resp = current_app.make_default_options_response()
-            else:
-                resp = make_response(f(*args, **kwargs))
-            if not attach_to_all and request.method != 'OPTIONS':
-                return resp
-
-            h = resp.headers
-
-            h['Access-Control-Allow-Origin'] = origin
-            h['Access-Control-Allow-Methods'] = get_methods()
-            h['Access-Control-Max-Age'] = str(max_age)
-            if headers is not None:
-                h['Access-Control-Allow-Headers'] = headers
-            return resp
-
-        f.provide_automatic_options = False
-        return update_wrapper(wrapped_function, f)
-    return decorator
-
 
 app = Flask(__name__)
 
@@ -185,7 +141,6 @@ def iidxdp10(user):
 	return render_score(player, score, "dp", "Beatmania IIDX DP lv.10 Rank")
 
 @app.route('/test')
-@crossdomain(origin='https://api.img.tl', headers='Content-Type')
 def test():
 	import json
 	f = open('./data/test', 'rb')
