5f4720502811ff349d9fe980d228d5f433701983
kuna <kgdgwn98@gmail.com>
2015-04-02 14:37:29 +0900

    layout fixed, main page created

---
+++ rankpage.py
@@ -0,0 +1,50 @@
+from flask import Flask, render_template, abort
+import iidx
+import jsondata
+import song
+
+def render_score(player, score, mode_, title_, titlehtml_):
+	name = ""
+	spdan = ""
+	dpdan = ""
+	if (player):
+		name = player['userdata']['djname']
+		spdan = player['userdata']['spclass']
+		dpdan = player['userdata']['dpclass']
+
+	return render_template('rankview.html', mode=mode_, title=title_, titlehtml=titlehtml_, \
+		user={'name': name, 'spdan': iidx.getdanstring(spdan), 'spdannum':spdan, 'dpdan': iidx.getdanstring(dpdan), 'dpdannum': dpdan},\
+		datas=score)
+
+def render_songlist(optionpath, user):
+	# read option json
+	option = jsondata.loadJSONfile(optionpath)
+
+	# if no user required, just render it without clear graph
+	# TODO
+	#if (user == None):
+	#	return render_score(None, score, option['type'], option['title'], option['titlehtml'])
+
+	# parse web (get player json file)
+	player = None
+	if (user == None):
+		data = jsondata.loadJSONfile(option['jsonfile'])
+	else:
+		player = song.getiidxinfo(user, option['type'], option['level'])
+
+	if (player == None or 'userdata' not in player or player['status'] != 'success'):
+		print "userdata not found"
+		abort(404)	# should show abort(404)
+		return
+
+	# load data file
+	data = song.getCSVdata(option['datafile'])
+	if (data == None):
+		print "level datafile not found"
+		abort(404)	# 404
+		return
+
+	# create score data
+	score = song.processCSV(player['musicdata'], data)
+
+	return render_score(player, score, option['type'], option['title'], option['titlehtml'])
--- web.py
+++ web.py
@@ -1,82 +1,52 @@
 #-*- coding: utf-8 -*-
 # start web server
 from flask import Flask, render_template, abort
-import song, view
 import env, private
 
 from flask import make_response, request, current_app
 
 import base64
-import iidx
-import jsondata
+import rankpage
 
 app = Flask(__name__)
-
-def render_score(player, score, mode_, title_, titlehtml_):
-	name = player['userdata']['djname']
-	spdan = player['userdata']['spclass']
-	dpdan = player['userdata']['dpclass']
-
-	return render_template('rankview.html', mode=mode_, title=title_, titlehtml=titlehtml_, \
-		user={'name': name, 'spdan': iidx.getdanstring(spdan), 'spdannum':spdan, 'dpdan': iidx.getdanstring(dpdan), 'dpdannum': dpdan},\
-		datas=score)
-
-def render_songlist(optionpath, user):
-	# read option json
-	option = jsondata.loadJSONfile(optionpath)
-
-	# parse web (get player json file)
-	player = None
-	if (user == None):
-		data = jsondata.loadJSONfile(option['jsonfile'])
-	else:
-		player = song.getiidxinfo(user, option['type'], option['level'])
-
-	if (player == None or 'userdata' not in player or player['status'] != 'success'):
-		abort(404)	# should show abort(404)
-		return
-
-	# load data file
-	data = song.getCSVdata(option['datafile'])
-	if (data == None):
-		abort(404)	# 404
-		return
-
-	# create score data
-	score = song.processCSV(player['musicdata'], data)
-
-	return render_score(player, score, option['type'], option['title'], option['titlehtml'])
 
 #####################################################
 
 @app.route('/iidx/sp/<user>/12')
 def iidxsp12(user):
-	return render_songlist("./data/sp.12.json", user)
-
-@app.route('/iidx/sp/<user>/12.7')
-def iidxsp127(user):
-	return render_songlist("./data/sp.12.7.json", user)
+	return rankpage.render_songlist("./data/sp.12.json", user)
 
 @app.route('/iidx/sp/<user>/11')
 def iidxsp11(user):
-	return render_songlist("./data/sp.11.json", user)
+	return rankpage.render_songlist("./data/sp.11.json", user)
 
 @app.route('/iidx/sp/<user>/10')
 def iidxsp10(user):
-	return render_songlist("./data/sp.10.json", user)
+	return rankpage.render_songlist("./data/sp.10.json", user)
 
 
 @app.route('/iidx/dp/<user>/12')
 def iidxdp12(user):
-	return render_songlist("./data/dp.12.json", user)
+	return rankpage.render_songlist("./data/dp.12.json", user)
 
 @app.route('/iidx/dp/<user>/11')
 def iidxdp11(user):
-	return render_songlist("./data/dp.11.json", user)
+	return rankpage.render_songlist("./data/dp.11.json", user)
 
 @app.route('/iidx/dp/<user>/10')
 def iidxdp10(user):
-	return render_songlist("./data/dp.10.json", user)
+	return rankpage.render_songlist("./data/dp.10.json", user)
+
+
+
+#@app.route('/iidx/sp/12.7')
+#def iidxsp127(user):
+#	return rankpage.render_songlist("./data/sp.12.7.json", None)
+
+
+@app.route('/test')
+def test():
+	return rankpage.render_songlist("./data/dp.10.json")
 
 #####################################################
 
@@ -88,12 +58,6 @@ def userpage(user):
 @app.route('/')
 def index():
 	return render_template('index.html')
-
-#####################################################
-
-@app.route('/test')
-def test():
-	return render_songlist("./data/dp.10.json")
 
 #####################################################
 
