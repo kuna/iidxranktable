106463c27be0d1d0a66a3f16eddc902612274745
kuna <kgdgwn98@gmail.com>
2015-12-14 01:52:23 +0900

    fixed bug: recent series song color won't change

--- iidxrank/static/js/ranklist.js
+++ iidxrank/static/js/ranklist.js
@@ -17,17 +17,22 @@ function ranklist(width, height, text, color, size) {
 		canvas.width = this.maxwidth;
 		var ctx = canvas.getContext("2d");
 
-		// default font!
+		// set font color/size/family of canvas
 		ctx.font = this.textsize + ' "M+ 1c medium"'
 		ctx.fillStyle = this.color;
+
+		// measure text (after font is selected)
 		var textWidth = ctx.measureText(this.text).width;
 
 		// set context size
 		if (textWidth > this.maxwidth) {
 			canvas.width = textWidth;
-			ctx.font = this.textsize + ' "M+ 1c medium"'	//reset font
 			canvas.setAttribute('style', "width:" + this.maxwidth + "px; height:" + this.height + "px;");
+			// must rest canvas information (it's reseted)
+			ctx.font = this.textsize + ' "M+ 1c medium"'
+			ctx.fillStyle = this.color;
 		}
+
 
 		// draw
 		ctx.fillText(this.text, 0, this.height - 4);
