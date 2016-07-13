b680e0f9a08fb6da7a6ff2a4e0f2d7918436093f
kuna <kgdgwn98@gmail.com>
2016-07-13 13:59:36 +0000

    it's time to migrate board db.

--- iidxrank/admin.py
+++ iidxrank/admin.py
@@ -4,14 +4,6 @@ from django.contrib import admin
 from django import forms
 import models
 
-# form
-class BoardCommentForm(forms.ModelForm):
-  class Meta:
-    model = models.BoardComment
-    fields = '__all__'
-    widgets = {
-      'text': forms.Textarea(attrs={'cols': 80, 'rows': 20}),
-    }
 class RankCategoryInline(admin.TabularInline):
   model = models.RankCategory
   ordering = ("-sortindex",)
@@ -34,26 +26,10 @@ class SongAdmin(admin.ModelAdmin):
   list_display = ('songtitle', 'songtype', 'songlevel')
   search_fields = ['songtitle', 'songtype']
 
-class BoardAdmin(admin.ModelAdmin):
-  list_display = ('title',)
-class BoardPostAdmin(admin.ModelAdmin):
-  list_display = ('writer', 'ip', 'get_boardtitle', 'title', 'tag')
-class BoardCommentAdmin(admin.ModelAdmin):
-  list_display = ('writer', 'ip', 'text', 'tag')
-  form = BoardCommentForm
-class SongCommentAdmin(admin.ModelAdmin):
-  list_display = ('writer', 'ip', 'get_songinfo', 'get_ranktableinfo',  'text')
-
 # register
 admin.site.register(models.RankTable, RankTableAdmin)
 admin.site.register(models.RankCategory, RankCategoryAdmin)
 admin.site.register(models.RankItem, RankItemAdmin)
 admin.site.register(models.Player, PlayerAdmin)
 admin.site.register(models.Song, SongAdmin)
-admin.site.register(models.SongComment, SongCommentAdmin)
 
-admin.site.register(models.Board, BoardAdmin)
-admin.site.register(models.BoardPost, BoardPostAdmin)
-admin.site.register(models.BoardComment, BoardCommentAdmin)
-admin.site.register(models.BannedUser)
-admin.site.register(models.BannedWord)
